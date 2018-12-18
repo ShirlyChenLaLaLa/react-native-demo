bbac635b5f54bdba94096b8f8d7c920e46fca419
Shirly Chen <1015885915@qq.com>
2018-12-18 15:01:20 +0800

    merry christmas

--- Merry.js
+++ Merry.js
@@ -6,7 +6,7 @@ import {
   Dimensions,
   Animated,
   Image,
-  Easing
+  Easing,
 } from 'react-native';
 import Snow from './imgs/snow.png';
 import SantaClaus from './imgs/12.png';
@@ -16,19 +16,19 @@ const { height: deviceHeight, width: deviceWidth } = Dimensions.get('window');
 const arr = [];
 const snowNumber = 50;
 for (var i = 0; i < snowNumber; i++) {
-	arr.push(i);
+  arr.push(i);
 }
 
 export default class DragSquare extends React.Component {
   constructor(props) {
     super(props);
     // this.state = {
-		// 	animatedValue: new Animated.Value(0),
-		// };
-		this.animateSnowmanValue = new Animated.Value(0);
-		this.animatedValue = [];
-		arr.forEach((value) => {
-      this.animatedValue[value] = new Animated.Value(0)
+    // 	animatedValue: new Animated.Value(0),
+    // };
+    this.animateSnowmanValue = new Animated.Value(0);
+    this.animatedValue = [];
+    arr.forEach(value => {
+      this.animatedValue[value] = new Animated.Value(0);
     });
   }
 
@@ -55,82 +55,79 @@ export default class DragSquare extends React.Component {
         this._animatedValue.flattenOffset();
       },
     });
-	}
-	
+  }
+
   componentDidMount() {
-		this.animateSnow();
-		this.animateSnowMan()
-	}
+    this.animateSnow();
+    this.animateSnowMan();
+  }
 
-	animateSnowMan () {
-		this.animateSnowmanValue.setValue(0);
-		Animated.timing(this.animateSnowmanValue, {
-			toValue:100,
+  animateSnowMan() {
+    this.animateSnowmanValue.setValue(0);
+    Animated.timing(this.animateSnowmanValue, {
+      toValue: 100,
       duration: 1000,
-      easing: Easing.linear
-		}).start(() => this.animateSnowMan());
-	}
+      easing: Easing.linear,
+    }).start(() => this.animateSnowMan());
+  }
 
-  animateSnow () {
-		arr.forEach((value) => {
-      this.animatedValue[value].setValue(0)
+  animateSnow() {
+    arr.forEach(value => {
+      this.animatedValue[value].setValue(0);
     });
-    const animations = arr.map((item) => {
-      return Animated.timing(
-        this.animatedValue[item],
-        {
-          toValue: snowNumber,
-          duration: 10000,
-          easing: Easing.linear
-        }
-      );
-    })
+    const animations = arr.map(item => {
+      return Animated.timing(this.animatedValue[item], {
+        toValue: snowNumber,
+        duration: 10000,
+        easing: Easing.linear,
+      });
+    });
     Animated.stagger(300, animations).start(() => this.animateSnow());
   }
-  
 
-	snowAnimate (item) {
-		return {
-			position: 'absolute',
-			left: parseInt(Math.random()*(deviceWidth),10),
-			top: this.animatedValue[item].interpolate({
-				inputRange: [0, snowNumber],
-				outputRange: [-100, deviceHeight-100],
-			}),
-			// transform: [{
-			// 	rotate: this.animatedValue[item].interpolate({
-			// 		inputRange: [0, 100],
-			// 		outputRange: ['0deg', '180deg']
-			// 	}),
-			// }]
-		}
-	}
+  snowAnimate(item) {
+    return {
+      position: 'absolute',
+      left: parseInt(Math.random() * deviceWidth, 10),
+      top: this.animatedValue[item].interpolate({
+        inputRange: [0, snowNumber],
+        outputRange: [-100, deviceHeight - 100],
+      }),
+      // transform: [{
+      // 	rotate: this.animatedValue[item].interpolate({
+      // 		inputRange: [0, 100],
+      // 		outputRange: ['0deg', '180deg']
+      // 	}),
+      // }]
+    };
+  }
 
   render() {
     let interpolatedRotation = this._animatedValue.x.interpolate({
       inputRange: [0, deviceWidth / 2, deviceHeight],
       outputRange: ['-360deg', '0deg', '360deg'],
-		});
-		const animateSnowMan = {
-			transform: [{
-				rotate: this.animateSnowmanValue.interpolate({
-					inputRange: [0, 50, 100],
-					outputRange: ['-50deg', '50deg', '-50deg'],	
-				}),
-      },{
-        translateX: 0.5
-      },{
-        translateY: 1,
-      }],
-		}
+    });
+    const animateSnowMan = {
+      transform: [
+        {
+          rotate: this.animateSnowmanValue.interpolate({
+            inputRange: [0, 50, 100],
+            outputRange: ['-50deg', '50deg', '-50deg'],
+          }),
+        },
+        {
+          translateX: 0.5,
+        },
+        {
+          translateY: 1,
+        },
+      ],
+    };
     const animations = arr.map((a, i) => {
       return (
-        <Animated.View
-          key={i}
-          style={[styles.snowBox, this.snowAnimate(a)]}
-        >
-					<Image source={Snow} style={styles.snow}/>
-				</Animated.View>
+        <Animated.View key={i} style={[styles.snowBox, this.snowAnimate(a)]}>
+          <Image source={Snow} style={styles.snow} />
+        </Animated.View>
       );
     });
 
@@ -151,17 +148,13 @@ export default class DragSquare extends React.Component {
         >
           <Image style={styles.image} source={SantaClaus} />
         </Animated.View>
-				{animations}
-				<Animated.View
-					style={styles.tree}
-				>
-					<Image style={styles.image} source={Tree} />
-				</Animated.View>
-				<Animated.View
-					style={[styles.snowMan, animateSnowMan]}
-				>
-					<Image style={styles.image} source={SnowMan} />
-				</Animated.View>
+        {animations}
+        <Animated.View style={styles.tree}>
+          <Image style={styles.image} source={Tree} />
+        </Animated.View>
+        <Animated.View style={[styles.snowMan, animateSnowMan]}>
+          <Image style={styles.image} source={SnowMan} />
+        </Animated.View>
       </View>
     );
   }
@@ -176,25 +169,25 @@ var styles = StyleSheet.create({
   box: {
     width: 50,
     height: 50,
-	},
-	tree: {
-		width: 200,
-		height: 200,
-	},
-	snowMan: {
-		width: 100,
-		height: 100,
-	},
+  },
+  tree: {
+    width: 200,
+    height: 200,
+  },
+  snowMan: {
+    width: 100,
+    height: 100,
+  },
   image: {
     width: '100%',
     height: '100%',
-	},
-	snowBox: {
-		width: 30,
-		height: 30,
-	},
-	snow: {
-		width: '100%',
-		height: '100%',
-	}
+  },
+  snowBox: {
+    width: 30,
+    height: 30,
+  },
+  snow: {
+    width: '100%',
+    height: '100%',
+  },
 });
